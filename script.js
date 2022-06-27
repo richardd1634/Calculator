b3ddee0061c42e1645af3e69c3542ca3187aab44
richardd1634 <richardd1634@gmail.com>
2022-06-27 13:47:09 -0500

    Add functions for basic operations in the script.js file

--- script.js
+++ script.js
@@ -1,4 +1,38 @@
 Calculator();
 function Calculator() {
-  const btn = document.querySelectorAll(".number .operator");
+  const btn = document.querySelectorAll(".number .operator .equal");
+
+  function Operate(num1, num2, operator) {
+    if (operator === "+") {
+      return Addition(num1, num2);
+    }
+
+    if (operator === "-") {
+      return Substraction(num1, num2);
+    }
+
+    if (operatior === "*") {
+      return Multiplication(num1, num2);
+    }
+
+    ir (operator === "/") {
+      return Division(num1, num2);
+    }
+  }
+
+  function Addition(num1, num2) {
+    return num1 + num2;
+  }
+
+  function Substraction(num1, num2) {
+    return num1 - num2;
+  }
+
+  function Multiplication(num1, num2) {
+    return num1 * num2;
+  }
+
+  function Division(num1, num2) {
+    return num1 / num2;
+  }
 }
