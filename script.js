d7032380741ef98861b36e0e56711762304f4cc4
richardd1634 <richardd1634@gmail.com>
2022-08-06 02:55:39 -0500

    Add click events for the numbers and operators.

--- script.js
+++ script.js
@@ -1,21 +1,83 @@
 Calculator();
 function Calculator() {
-  const btn = document.querySelectorAll(".number .operator .equal");
+  const history = document.getElementById("history");
+  const display = document.getElementById("main-display");
+  const numberBtns = document.querySelectorAll(".number");
+  const operatorBtns = document.querySelectorAll(".operator");
+  const pointBtn = document.getElementById("point");
+  const equalBtn = document.getElementById("equal");
+  const clearBtn = document.getElementById("clear");
+  const numRegex = /\d/g;
+  let operand1,
+    operand2,
+    operator = 0;
 
-  function Operate(num1, num2, operator) {
-    if (operator === "+") {
+  clearBtn.addEventListener("click", () => {
+    history.textContent = "";
+    ClearDisplay();
+  });
+
+  function ClearDisplay() {
+    display.textContent = "0";
+    pointBtn.disabled = false;
+  }
+
+  numberBtns.forEach((numberBtn) => {
+    numberBtn.addEventListener("click", (event) =>
+      NumberBtnClicked(event.currentTarget.textContent)
+    );
+  });
+
+  function NumberBtnClicked(btnValue) {
+    if (display.textContent === "0") {
+      display.textContent = "";
+    }
+
+    // Limit the amount of digit to 16
+    if (display.textContent.length < 16) {
+      display.textContent += btnValue;
+    }
+
+    // Disable the point button after one hit
+    if (btnValue === ".") {
+      pointBtn.disabled = true;
+    }
+  }
+
+  operatorBtns.forEach((operatorBtn) => {
+    operatorBtn.addEventListener("click", (event) =>
+      OperatorBtnClicked(event.currentTarget.textContent)
+    );
+  });
+
+  function OperatorBtnClicked(btnValue) {
+    operator = btnValue;
+    const numbersArray =
+      history.textContent !== "" ? history.textContent.match(numRegex) : ["0"];
+    operand1 = Number(numbersArray.join(""));
+    operand2 = Number(display.textContent);
+    history.textContent = `${Operate(
+      operand1,
+      operand2,
+      operator
+    )} ${operator}`;
+    ClearDisplay();
+  }
+
+  function Operate(num1, num2, operation) {
+    if (operation === "+") {
       return Addition(num1, num2);
     }
 
-    if (operator === "-") {
+    if (operation === "-") {
       return Substraction(num1, num2);
     }
 
-    if (operatior === "*") {
+    if (operation === "*") {
       return Multiplication(num1, num2);
     }
 
-    if (operator === "/") {
+    if (operation === "/") {
       return Division(num1, num2);
     }
   }
